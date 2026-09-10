4add126c7ae1f0888c5ec041b6017ba640b1e3df
Becerrin08 <crisbecerra580@gmail.com>
2026-09-10 11:06:06 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Create_Roise',
+  webDir: 'www'
+};
+
+export default config;
